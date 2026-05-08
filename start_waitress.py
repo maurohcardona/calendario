c6b48232546072a6e7d6ca5023ef13ed4a396c4e
Mauro <maurohcardona@gmail.com>
2026-05-08 12:58:44 -0300

    se modifico el start.bat

--- start_waitress.py
+++ start_waitress.py
@@ -3,5 +3,6 @@ from Agenda.wsgi import application
 
 if __name__ == "__main__":
     print("Iniciando waitress...")
-    serve(application, host="0.0.0.0", port=8000)
+    print("Servidor iniciado en puerto 8000 con 8 threads")
+    serve(application, host="0.0.0.0", port=8000, threads=8)
     print("Waitress finalizó")
